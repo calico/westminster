7a53e1040fea33a47351fa2bd2e740ac056751c2
David Kelley <drk@calicolabs.com>
2023-04-26 11:21:53 -0700

    tissue subsets

--- scripts/westminster_gtex_coef.py
+++ scripts/westminster_gtex_coef.py
@@ -2,6 +2,7 @@
 import argparse
 import os
 import pdb
+import re
 
 import h5py
 import numpy as np
@@ -31,7 +32,7 @@ def main():
   args = parser.parse_args()
 
   os.makedirs(args.out_dir, exist_ok=True)
-
+  
   tissue_keywords = {
       'Adipose_Subcutaneous': 'adipose',
       'Adipose_Visceral_Omentum': 'adipose',
@@ -62,38 +63,41 @@ def main():
   }
   # 'Cells_Cultured_fibroblasts': 'fibroblast',
  
-  sign_auroc_list = []
-  coef_r_list = []
+  metrics_tissue = []
+  metrics_auroc = []
+  metrics_r = []
   for tissue, keyword in tissue_keywords.items():
     if args.verbose: print(tissue)
 
     # read causal variants
     eqtl_df = read_eqtl(tissue, args.gtex_vcf_dir)
+    if eqtl_df is not None:
+      # read model predictions
+      gtex_scores_file = f'{args.gtex_dir}/{tissue}_pos/sad.h5'
+      variant_scores = read_scores(gtex_scores_file, keyword,
+                                   eqtl_df, verbose=args.verbose)
+      variant_scores = variant_scores[eqtl_df.consistent]
 
-    # read model predictions
-    variant_scores = read_scores(tissue, keyword, args.gtex_dir,
-                                 eqtl_df, verbose=args.verbose)
-    variant_scores = variant_scores[eqtl_df.consistent]
+      # compute AUROCs
+      variant_sign = eqtl_df[eqtl_df.consistent].sign
+      sign_auroc = roc_auc_score(variant_sign, variant_scores)
 
-    # compute AUROCs
-    variant_sign = eqtl_df[eqtl_df.consistent].sign
-    sign_auroc = roc_auc_score(variant_sign, variant_scores)
+      # compute PearsonR
+      variant_coef = eqtl_df[eqtl_df.consistent].coef
+      coef_r = spearmanr(variant_coef, variant_scores)[0]
 
-    # compute PearsonR
-    variant_coef = eqtl_df[eqtl_df.consistent].coef
-    coef_r = spearmanr(variant_coef, variant_scores)[0]
+      # save
+      metrics_tissue.append(tissue)
+      metrics_auroc.append(sign_auroc)
+      metrics_r.append(coef_r)
 
-    # save
-    sign_auroc_list.append(sign_auroc)
-    coef_r_list.append(coef_r)
-
-    if args.verbose: print('')
+      if args.verbose: print('')
 
   # save metrics
   metrics_df = pd.DataFrame({
-      'tissue': tissue_keywords.keys(),
-      'auroc': sign_auroc_list,
-      'spearmanr': coef_r_list
+      'tissue': metrics_tissue,
+      'auroc': metrics_auroc,
+      'spearmanr': metrics_r
   })
   metrics_df.to_csv(f'{args.out_dir}/metrics.tsv',
                     sep='\t', index=False, float_format='%.4f')
@@ -112,13 +116,18 @@ def read_eqtl(tissue: str, gtex_vcf_dir: str, pip_t: float=0.9):
     pip_t (float): PIP threshold.
 
   Returns:
-    eqtl_df (pd.DataFrame): eQTL dataframe.
+    eqtl_df (pd.DataFrame): eQTL dataframe, or None if tissue skipped.
   """
   susie_dir = '/home/drk/seqnn/data/gtex_fine/tissues_susie'
 
   # read causal variants
   eqtl_file = f'{susie_dir}/{tissue}.tsv'
   df_eqtl = pd.read_csv(eqtl_file, sep='\t', index_col=0)
+
+  # pip filter
+  pip_match = re.search(r"_pip(\d+).?$", gtex_vcf_dir).group(1)
+  pip_t = float(pip_match) / 100
+  assert(pip_t > 0 and pip_t <= 1)
   df_causal = df_eqtl[df_eqtl.pip > pip_t]
 
   # remove variants with inconsistent signs
@@ -137,12 +146,6 @@ def read_eqtl(tissue: str, gtex_vcf_dir: str, pip_t: float=0.9):
         inconsistent_variants.add(vid)
     else:
       variant_sign[vid] = vsign
-
-  # delete inconsistent variants  
-  # for vid in inconsistent_variants:
-  #   del variant_sign[vid]
-  #   del variant_z[vid]
-  #   del variant_a1[vid]
       
   # average z-scores across genes
   for vid in variant_z:
@@ -150,38 +153,33 @@ def read_eqtl(tissue: str, gtex_vcf_dir: str, pip_t: float=0.9):
 
   # order variants
   tissue_vcf_file = f'{gtex_vcf_dir}/{tissue}_pos.vcf'
-  pred_variants = np.array([line.split()[2] for line in open(tissue_vcf_file) if not line.startswith('##')])
-  consistent_mask = np.array([vid not in inconsistent_variants for vid in pred_variants])
+  if not os.path.isfile(tissue_vcf_file):
+    eqtl_df = None
+  else:
+    pred_variants = np.array([line.split()[2] for line in open(tissue_vcf_file) if not line.startswith('##')])
+    consistent_mask = np.array([vid not in inconsistent_variants for vid in pred_variants])
 
-  # create dataframe
-  # eqtl_df = pd.DataFrame({
-  #   'variant': pred_variants[sign_mask],
-  #   'coef': [variant_z[vid] for vid in pred_variants[sign_mask]],
-  #   'sign': [variant_sign[vid] for vid in pred_variants[sign_mask]],
-  #   'allele': [variant_a1[vid] for vid in pred_variants[sign_mask]]
-  # })
-  eqtl_df = pd.DataFrame({
-    'variant': pred_variants,
-    'coef': [variant_z[vid] for vid in pred_variants],
-    'sign': [variant_sign[vid] for vid in pred_variants],
-    'allele': [variant_a1[vid] for vid in pred_variants],
-    'consistent': consistent_mask
-  })
+    # create dataframe
+    eqtl_df = pd.DataFrame({
+      'variant': pred_variants,
+      'coef': [variant_z[vid] for vid in pred_variants],
+      'sign': [variant_sign[vid] for vid in pred_variants],
+      'allele': [variant_a1[vid] for vid in pred_variants],
+      'consistent': consistent_mask
+    })
   return eqtl_df
 
 
-def read_scores(tissue: str,
+def read_scores(gtex_scores_file: str,
                 keyword: str,
-                gtex_dir: str,
                 eqtl_df: pd.DataFrame,
                 score_key: str='SAD',
                 verbose: bool=False):
   """Read eQTL RNA predictions for the given tissue.
   
   Args:
-    tissue (str): tissue name
+    gtex_scores_file (str): Variant scores HDF5.
     tissue_keyword (str): tissue keyword, for matching GTEx targets
-    gtex_dir (str): directory containing eQTL predictions
     eqtl_df (pd.DataFrame): eQTL dataframe
     score_key (str): score key in HDF5 file
     verbose (bool): Print matching targets.
@@ -189,7 +187,6 @@ def read_scores(tissue: str,
   Returns:
     np.array: eQTL predictions
   """
-  gtex_scores_file = f'{gtex_dir}/{tissue}_pos/sad.h5'
   with h5py.File(gtex_scores_file, 'r') as gtex_scores_h5:
     score_ref = np.array([ref.decode('UTF-8') for ref in gtex_scores_h5['ref_allele']])
     
