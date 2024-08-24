5e259d7296c3734bf405401928b577bbd9028fab
David Kelley <drk@calicolabs.com>
2024-08-23 18:03:07 -0700

    check for model

--- src/westminster/scripts/westminster_eval_folds.py
+++ src/westminster/scripts/westminster_eval_folds.py
@@ -151,7 +151,7 @@ def main():
         options.fold_subset = num_folds
 
     if options.queue == "standard":
-        num_cpu = 8
+        num_cpu = 16
         num_gpu = 0
         time_base = 64
     else:
@@ -175,50 +175,52 @@ def main():
                 else:
                     eval_dir = f"{it_dir}/eval{di}"
                     model_file = f"{it_dir}/train/model{di}_best.h5"
-                os.makedirs(eval_dir, exist_ok=True)
+        
+                if os.path.isfile(model_file):
+                    os.makedirs(eval_dir, exist_ok=True)
 
-                for ei in range(num_folds):
-                    eval_fold_dir = f"{eval_dir}/fold{ei}"
+                    for ei in range(num_folds):
+                        eval_fold_dir = f"{eval_dir}/fold{ei}"
 
-                    # symlink test metrics
-                    if fi == ei and not os.path.isfile(f"{eval_fold_dir}/test"):
-                        os.symlink(f"{eval_fold_dir}", f"{eval_dir}/test")
-                        os.symlink(f"{eval_fold_dir}.out", f"{eval_dir}/test.out")
+                        # symlink test metrics
+                        if fi == ei and not os.path.isfile(f"{eval_dir}/test.out"):
+                            os.symlink(f"fold{ei}", f"{eval_dir}/test")
+                            os.symlink(f"fold{ei}.out", f"{eval_dir}/test.out")
 
-                    # check if done
-                    acc_file = f"{eval_fold_dir}/acc.txt"
-                    if os.path.isfile(acc_file):
-                        print("%s already generated." % acc_file)
-                    else:
-                        # hound evaluate
-                        cmd = ". /home/drk/anaconda3/etc/profile.d/conda.sh;"
-                        cmd += " conda activate %s;" % options.conda_env
-                        cmd += " echo $HOSTNAME;"
-                        cmd += " hound_eval.py"
-                        cmd += " --head %d" % di
-                        cmd += " -o %s" % eval_fold_dir
-                        if options.rc:
-                            cmd += " --rc"
-                        if options.shifts:
-                            cmd += " --shifts %s" % options.shifts
-                        cmd += " --split fold%d" % ei
-                        cmd += " %s" % params_file
-                        cmd += " %s" % model_file
-                        cmd += " %s" % data_dirs[di]
+                        # check if done
+                        acc_file = f"{eval_fold_dir}/acc.txt"
+                        if os.path.isfile(acc_file):
+                            print("%s already generated." % acc_file)
+                        else:
+                            # hound evaluate
+                            cmd = ". /home/drk/anaconda3/etc/profile.d/conda.sh;"
+                            cmd += " conda activate %s;" % options.conda_env
+                            cmd += " echo $HOSTNAME;"
+                            cmd += " hound_eval.py"
+                            cmd += " --head %d" % di
+                            cmd += " -o %s" % eval_fold_dir
+                            if options.rc:
+                                cmd += " --rc"
+                            if options.shifts:
+                                cmd += " --shifts %s" % options.shifts
+                            cmd += " --split fold%d" % ei
+                            cmd += " %s" % params_file
+                            cmd += " %s" % model_file
+                            cmd += " %s" % data_dirs[di]
 
-                        name = "%s-eval-f%de%d" % (options.name, fi, ei)
-                        job = slurm.Job(
-                            cmd,
-                            name=name,
-                            out_file=f"{eval_fold_dir}.out",
-                            err_file=f"{eval_fold_dir}.err",
-                            queue=options.queue,
-                            cpu=num_cpu,
-                            gpu=num_gpu,
-                            mem=30000,
-                            time="%d:00:00" % time_base,
-                        )
-                        jobs.append(job)
+                            name = "%s-eval-f%de%d" % (options.name, fi, ei)
+                            job = slurm.Job(
+                                cmd,
+                                name=name,
+                                out_file=f"{eval_fold_dir}.out",
+                                err_file=f"{eval_fold_dir}.err",
+                                queue=options.queue,
+                                cpu=num_cpu,
+                                gpu=num_gpu,
+                                mem=30000,
+                                time="%d:00:00" % time_base,
+                            )
+                            jobs.append(job)
 
     #######################################################
     # evaluate test specificity
@@ -236,39 +238,40 @@ def main():
                         out_dir = "%s/spec%d" % (it_dir, di)
                         model_file = "%s/train/model%d_best.h5" % (it_dir, di)
 
-                    # check if done
-                    acc_file = "%s/acc.txt" % out_dir
-                    if os.path.isfile(acc_file):
-                        print("%s already generated." % acc_file)
-                    else:
-                        cmd = ". /home/drk/anaconda3/etc/profile.d/conda.sh;"
-                        cmd += " conda activate %s;" % options.conda_env
-                        cmd += " echo $HOSTNAME;"
-                        cmd += " hound_eval_spec.py"
-                        cmd += " --head %d" % di
-                        cmd += " -o %s" % out_dir
-                        cmd += " --step %d" % options.step
-                        if options.rc:
-                            cmd += " --rc"
-                        if options.shifts:
-                            cmd += " --shifts %s" % options.shifts
-                        cmd += " %s" % params_file
-                        cmd += " %s" % model_file
-                        cmd += " %s/data%d" % (it_dir, di)
+                    if os.path.isfile(model_file):
+                        # check if done
+                        acc_file = "%s/acc.txt" % out_dir
+                        if os.path.isfile(acc_file):
+                            print("%s already generated." % acc_file)
+                        else:
+                            cmd = ". /home/drk/anaconda3/etc/profile.d/conda.sh;"
+                            cmd += " conda activate %s;" % options.conda_env
+                            cmd += " echo $HOSTNAME;"
+                            cmd += " hound_eval_spec.py"
+                            cmd += " --head %d" % di
+                            cmd += " -o %s" % out_dir
+                            cmd += " --step %d" % options.step
+                            if options.rc:
+                                cmd += " --rc"
+                            if options.shifts:
+                                cmd += " --shifts %s" % options.shifts
+                            cmd += " %s" % params_file
+                            cmd += " %s" % model_file
+                            cmd += " %s/data%d" % (it_dir, di)
 
-                        name = "%s-spec-f%dc%d" % (options.name, fi, ci)
-                        job = slurm.Job(
-                            cmd,
-                            name=name,
-                            out_file="%s.out" % out_dir,
-                            err_file="%s.err" % out_dir,
-                            queue=options.queue,
-                            cpu=1,
-                            gpu=1,
-                            mem=80000,
-                            time="%d:00:00" % (3 * time_base),
-                        )
-                        jobs.append(job)
+                            name = "%s-spec-f%dc%d" % (options.name, fi, ci)
+                            job = slurm.Job(
+                                cmd,
+                                name=name,
+                                out_file="%s.out" % out_dir,
+                                err_file="%s.err" % out_dir,
+                                queue=options.queue,
+                                cpu=num_cpu,
+                                gpu=num_gpu,
+                                mem=90000,
+                                time="%d:00:00" % (3 * time_base),
+                            )
+                            jobs.append(job)
 
     slurm.multi_run(
         jobs, max_proc=options.processes, verbose=True, launch_sleep=10, update_sleep=60
